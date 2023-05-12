5d963970ea091815ae0ffc0f9659d2592db39384
LucasFadavi91 <71818752+LucasFadavi91@users.noreply.github.com>
2023-05-12 03:36:30 +0200

    Add files via upload

---
+++ tiendaVideoJuegos/src/test/java/com/lfsdesign/tiendaVideoJuegos/TiendaVideoJuegosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lfsdesign.tiendaVideoJuegos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaVideoJuegosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
