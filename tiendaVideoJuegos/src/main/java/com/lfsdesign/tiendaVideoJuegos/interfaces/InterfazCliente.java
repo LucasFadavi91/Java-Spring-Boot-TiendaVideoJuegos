a76289bf870befec94a9d197b3623a1cfc778b19
LucasFadavi91 <71818752+LucasFadavi91@users.noreply.github.com>
2023-05-17 02:56:30 +0200

    Add files via upload
    includes the JuegoControladorTest and ClienteControladorTest classes and include a general refactoring of the API.

---
+++ tiendaVideoJuegos/src/main/java/com/lfsdesign/tiendaVideoJuegos/interfaces/InterfazCliente.java
@@ -0,0 +1,20 @@
+package com.lfsdesign.tiendaVideoJuegos.interfaces;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.lfsdesign.tiendaVideoJuegos.models.Cliente;
+
+public interface InterfazCliente {
+
+	int insertarCliente(Cliente cliente);
+	
+	List<Cliente> selectAllClientes();
+	
+	Optional<Cliente> selectClienteByDni(String dni);
+	
+	int borrarClienteByDni(String dni);
+	
+	int actualizarClienteByDni(String dni, Cliente clienteActualizado);
+
+}
